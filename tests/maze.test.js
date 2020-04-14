93ba1cee1dc0bf98a5715785600b93e17090d3d6
Matthew Riley <matthew@matthewriley.name>
2020-04-14 11:43:19 +1000

    safe exit reached when only 1 column of empty desks

--- tests/maze.test.js
+++ tests/maze.test.js
@@ -16,9 +16,22 @@ test('Already at safe exit when only 1 desk', t => {
     t.true(safeExitFound);
 });
 
+test('Safe exit reached when only 1 column of empty desks', t => {
+    t.plan(1);
+    const row0 = [0];
+    const row1 = [0];
+    const row2 = [0];
+    const office = [row0, row1, row2];
+    const safeExitFound = findSafeExit(office);
+    t.true(safeExitFound);
+});
+
 const findSafeExit = office => {
     if (!office.length) {
         throw new Error('Office without desks');
     }
+
+    let y = office.length - 1;
+    while (y > 0) y--;
     return true;
 };
