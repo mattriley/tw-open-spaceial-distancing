6f26395270145507cb70b995d3708d6f2b3c3e6d
Matthew Riley <matthew@matthewriley.name>
2020-04-14 13:22:41 +1000

    refactor to one-based instead of zero-based row numbering for readability

--- tests/find-safe-exit.test.js
+++ tests/find-safe-exit.test.js
@@ -11,28 +11,28 @@ test('Office without desks is not a valid scenario', t => {
 
 test('Already at safe exit when only 1 desk', t => {
     t.plan(1);
-    const row0 = [0];
-    const office = [row0];
+    const row1 = [0];
+    const office = [row1];
     const safeExitFound = findSafeExit(office);
     t.true(safeExitFound);
 });
 
 test('Safe exit reached when only 1 column of empty desks', t => {
     t.plan(1);
-    const row0 = [0];
     const row1 = [0];
     const row2 = [0];
-    const office = [row0, row1, row2];
+    const row3 = [0];
+    const office = [row1, row2, row3];
     const safeExitFound = findSafeExit(office);
     t.true(safeExitFound);
 });
 
 test('Safe exit blocked when only 1 column of empty desks', t => {
     t.plan(1);
-    const row0 = [0];
     const row1 = [0];
-    const row2 = [1]; // blocked
-    const office = [row0, row1, row2];
+    const row2 = [0];
+    const row3 = [1]; // blocked
+    const office = [row1, row2, row3];
     const safeExitFound = findSafeExit(office);
     t.false(safeExitFound);
 });
