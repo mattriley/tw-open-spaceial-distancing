7a51054cebf7536a98f661f523e564062776cf1e
Matthew Riley <matthew@matthewriley.name>
2020-04-14 11:19:43 +1000

    already at safe exit when only 1 desk

--- tests/maze.test.js
+++ tests/maze.test.js
@@ -8,6 +8,17 @@ test('Office without desks is not a valid scenario', t => {
     }, 'Office without desks');
 });
 
-const findSafeExit = () => {
-    throw new Error('Office without desks');
+test('Already at safe exit when only 1 desk', t => {
+    t.plan(1);
+    const row0 = [0];
+    const office = [row0];
+    const safeExitFound = findSafeExit(office);
+    t.true(safeExitFound);
+});
+
+const findSafeExit = office => {
+    if (!office.length) {
+        throw new Error('Office without desks');
+    }
+    return true;
 };
