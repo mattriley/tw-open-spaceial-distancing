1da7a7211c6b9d1636b3fd46fe1537c040bb7afb
Matthew Riley <matthew@matthewriley.name>
2020-04-15 10:20:44 +1000

    refactor to reduce code size

--- src/find-safe-exit.js
+++ src/find-safe-exit.js
@@ -1,10 +1,11 @@
 const drawOffice = require('./draw-office');
 
-const status = {
-    unoccupied: 0,
-    occupied: 1,
-    visited: 2
-};
+const unoccupied = 0;
+const occupied = 1;
+const visited = 2;
+
+const offsets = [{ row: -1 }, { row: 1 }, { col: -1 }, { col: 1 }];
+const offsetDefaults = { row: 0, col: 0 };
 
 module.exports = (office, startCol = 0) => {
     if (!office.length) {
@@ -14,21 +15,18 @@ module.exports = (office, startCol = 0) => {
     const startRow = office.length - 1;
     const exitRow = 0;
 
-    const offsets = [{ rowIndexOffset: -1 }, { rowIndexOffset: 1 }, { colIndexOffset: -1 }, { colIndexOffset: 1 }];
-
     const findExit = (row, col) => {
         const desk = office[row][col];
-        if (desk == status.unoccupied && row == exitRow) return true;
-        if (desk == status.occupied || desk == status.visited) return false;
-        office[row][col] = 2;
+        if (desk == unoccupied && row == exitRow) return true;
+        if (desk == occupied || desk == visited) return false;
+        office[row][col] = visited;
 
         return offsets.find(offset => {
-            const defaults = { rowIndexOffset: 0, colIndexOffset: 0 };
-            const { rowIndexOffset, colIndexOffset } = { ...defaults, ...offset };
-            const nextRowIndex = row + rowIndexOffset;
-            const nextColIndex = col + colIndexOffset;
-            const nextDesk = office[nextRowIndex] && office[nextRowIndex][nextColIndex];
-            return nextDesk !== undefined ? findExit(nextRowIndex, nextColIndex) : false;
+            const o = { ...offsetDefaults, ...offset };
+            const nextRow = row + o.row;
+            const nextCol = col + o.col;
+            const nextDesk = office[nextRow] && office[nextRow][nextCol];
+            return nextDesk !== undefined ? findExit(nextRow, nextCol) : false;
         });
     };
 
