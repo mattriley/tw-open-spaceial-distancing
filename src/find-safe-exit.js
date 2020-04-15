b3fce7f5b0aa8002d6d1d622a17816c265e23940
Matthew Riley <matthew@matthewriley.name>
2020-04-15 11:34:58 +1000

    refactor to reduce code by representing visited desk as undefined

--- src/find-safe-exit.js
+++ src/find-safe-exit.js
@@ -1,17 +1,15 @@
 const exitRow = 0;
 const unoccupied = 0;
 const occupied = 1;
-const visited = 2;
 const offsets = [-1, 1];
 
 module.exports = (office, startCol = 0) => {
     const findExit = pos => {
         const { row, col } = pos;
         const desk = office[row] && office[row][col];
-        if (desk == undefined) return false;
+        if (desk == undefined || desk == occupied) return false;
         if (desk == unoccupied && row == exitRow) return true;
-        if (desk == occupied || desk == visited) return false;
-        office[row][col] = visited;
+        delete office[row][col];
         const applyOffset = (key, val, offset) => ({ row, col, [key]: val + offset });
         const adjacentDesks = Object.entries(pos).flatMap(([key, val]) => offsets.map(offset => applyOffset(key, val, offset)));
         return adjacentDesks.find(findExit);
